484d52bf711b20befecd644ad89c74e41c96cd21
Dimitri Jorge <jorge.dimitri@gmail.com>
2016-01-26 19:47:28 +0000

    Allow reload of dictionnary

--- lib/obscenity/config.rb
+++ lib/obscenity/config.rb
@@ -1,41 +1,55 @@
 module Obscenity
   class Config
-    
+
     attr_accessor :replacement
-    
+
     DEFAULT_WHITELIST = []
     DEFAULT_BLACKLIST = File.dirname(__FILE__) + "/../../config/blacklist.yml"
-    
+
     def initialize
+      @init = true
       yield(self) if block_given?
+      @init = false
+
       validate_config_options
+      update_base_lists
     end
-    
+
     def replacement
       @replacement ||= :garbled
     end
-    
+
     def blacklist
       @blacklist ||= DEFAULT_BLACKLIST
     end
-    
+
     def blacklist=(value)
       @blacklist = value == :default ? DEFAULT_BLACKLIST : value
+      if @init
+        @update_blacklist = true
+      else
+        Base.blacklist = set_list_content(@blacklist)
+      end
     end
-    
+
     def whitelist
       @whitelist ||= DEFAULT_WHITELIST
     end
-    
+
     def whitelist=(value)
       @whitelist = value == :default ? DEFAULT_WHITELIST : value
+      if @init
+        @update_whitelist = true
+      else
+        Base.whitelist = set_list_content(@whitelist)
+      end
     end
-    
+
     private
     def validate_config_options
       [@blacklist, @whitelist].each{ |content| validate_list_content(content) if content }
     end
-    
+
     def validate_list_content(content)
       case content
       when Array    then !content.empty?       || raise(Obscenity::EmptyContentList.new('Content array is empty.'))
@@ -46,6 +60,19 @@ module Obscenity
         raise Obscenity::UnkownContent.new("The content can be either an Array, Pathname, or String path to a .yml file.")
       end
     end
-    
+
+    def update_base_lists
+      Base.blacklist = set_list_content(@blacklist) if @update_blacklist
+      Base.whitelist = set_list_content(@whitelist) if @update_whitelist
+    end
+
+    def set_list_content(list)
+      case list
+      when Array then list
+      when String, Pathname then YAML.load_file( list.to_s )
+      else []
+      end
+    end
+
   end
 end
--- test/test_config.rb
+++ test/test_config.rb
@@ -11,18 +11,18 @@ class TestConfig < Test::Unit::TestCase
       end
     end
   end
-  
+
   should "properly set the config parameters" do
     blacklist   = ['ass', 'shit', 'penis']
     whitelist   = ['penis']
     replacement = :stars
-    
+
     config = Obscenity::Config.new do |config|
       config.blacklist   = blacklist
       config.whitelist   = whitelist
       config.replacement = replacement
     end
-    
+
     assert_equal blacklist, config.blacklist
     assert_equal whitelist, config.whitelist
     assert_equal replacement, config.replacement
@@ -44,14 +44,14 @@ class TestConfig < Test::Unit::TestCase
     assert_equal :default, config.replacement
     assert_match /config\/blacklist.yml/, config.blacklist
   end
-  
+
   should "properly validate the config options" do
     [:blacklist, :whitelist].each do |field|
       exceptions = [
-        [Obscenity::UnkownContent, {}], 
-        [Obscenity::UnkownContent, ":unkown"], 
-        [Obscenity::EmptyContentList, []], 
-        [Obscenity::UnkownContentFile, "'path/to/file'"], 
+        [Obscenity::UnkownContent, {}],
+        [Obscenity::UnkownContent, ":unkown"],
+        [Obscenity::EmptyContentList, []],
+        [Obscenity::UnkownContentFile, "'path/to/file'"],
         [Obscenity::UnkownContentFile, Pathname.new("'path/to/file'")]
       ].each do |klass, value|
         assert_raise(klass){
@@ -62,5 +62,61 @@ class TestConfig < Test::Unit::TestCase
       end
     end
   end
-  
+
+  should "change blacklist via Obscenity.configure block" do
+    Obscenity.configure do |config|
+      config.blacklist = ['fuck']
+      config.whitelist = nil
+    end
+
+    assert_equal true, Obscenity.profane?('fuck')
+    assert_equal false, Obscenity.profane?('shit')
+
+    Obscenity.configure do |config|
+      config.blacklist = ['shit']
+    end
+
+    assert_equal false, Obscenity.profane?('fuck')
+    assert_equal true, Obscenity.profane?('shit')
+  end
+
+  should "change blacklist via Obscenity.config" do
+    Obscenity.config.blacklist = ['fuck']
+    Obscenity.config.whitelist = nil
+
+    assert_equal true, Obscenity.profane?('fuck')
+    assert_equal false, Obscenity.profane?('shit')
+
+    Obscenity.config.blacklist = ['shit']
+
+    assert_equal false, Obscenity.profane?('fuck')
+    assert_equal true, Obscenity.profane?('shit')
+  end
+
+  should "change whitelist via Obscenity.configure block" do
+    Obscenity.configure do |config|
+      config.blacklist = ['fuck']
+      config.whitelist = nil
+    end
+
+    assert_equal true, Obscenity.profane?('fuck')
+
+    Obscenity.configure do |config|
+      config.whitelist = ['fuck']
+    end
+
+    assert_equal false, Obscenity.profane?('fuck')
+  end
+
+  should "change whitelist via Obscenity.config" do
+    Obscenity.config.blacklist = ['fuck']
+    Obscenity.config.whitelist = nil
+
+    assert_equal true, Obscenity.profane?('fuck')
+
+    Obscenity.config.whitelist = ['fuck']
+
+    assert_equal false, Obscenity.profane?('fuck')
+  end
+
 end
